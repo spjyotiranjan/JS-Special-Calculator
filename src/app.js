45e6e97ae5ce7bbd109993101f73abe0cbcc92ad
spjyotiranjan <jyotiranjan.sahoo@kalvium.community>
2023-11-20 18:09:50 +0530

    Done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,31 @@
 // Progression 1: Create special addition
 
 // function specialAddition(){}
+function specialAddition(numberOne, numberTwo) {
+    if (numberOne < 0 || numberTwo < 0) {
+        return "Negative numbers are not allowed"
+    } else {
+        return numberOne - numberTwo
+    }
+}
 
 // Progression 2: Create simple division
 
+function simpleDivision(numberOne, numberTwo) {
+    if (numberOne<numberTwo) {
+        return "Cannot divide a smaller number by a larger number"
+    } else if(numberOne < 0 || numberTwo < 0) {
+        return "Negative numbers are not allowed"
+    }
+    else if(numberTwo == 0){
+        return "Not a number"
+    }else{
+        return numberOne/numberTwo
+    }
+}
+
 // Progression 3: Create the special calculator
+
+function specialCalculator(numberOne, numberTwo, operation) {
+    return operation(numberOne,numberTwo)
+}
